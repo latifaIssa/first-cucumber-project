37df7e6ed2063bf5f68d3a4797b0ea69e2c5e176
Latifa Masri <lmasri@asaltech.com>
2022-09-14 11:56:46 +0300

    Generate report

--- src/test/java/stepDefinisions/Runner.java
+++ src/test/java/stepDefinisions/Runner.java
@@ -5,6 +5,14 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features", glue= {"stepDefinisions"})
+@CucumberOptions(features="src/test/resources/features",
+        glue= {"stepDefinisions"},
+        monochrome = true,
+        //we can create folder or file to the report
+//        plugin = {"pretty","html:target/HtmlReports"}
+//        plugin = {"pretty","json:target/JsonReports"}
+        // first we should create subfolder JUnitReports
+        plugin = {"pretty","junit:target/JUnitReports/report.xml"}
+)
 public class Runner {
 }
