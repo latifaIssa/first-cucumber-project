cc980199d09d83b389f81be11d5b2daed2c2e3f2
Latifa Masri <lmasri@asaltech.com>
2022-09-19 11:57:17 +0300

    Parameterization & Data Driven Testing for login

---
+++ src/test/java/stepDefinisions/LoginTestSteps.java
@@ -0,0 +1,55 @@
+package stepDefinisions;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+
+public class LoginTestSteps {
+
+    WebDriver driver = null;
+
+    @Given("Browser is open.")
+    public void browser_is_open() {
+        System.out.println("Inside-Step: Browser is open.");
+        System.setProperty("webdriver.chrome.driver","C:/Users/lmasri/Documents/Cucumber-project/first_cucumber/src/test/resources/Drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+    }
+
+    @And("user is on login page")
+    public void user_is_on_login_page() {
+        System.out.println("Inside-Step: user is on login page");
+        driver.navigate().to("https://example.testproject.io/web/");
+    }
+
+    // we can use (.*) instead of {} to show that any string here and (\\d+) for any integer
+    //    use ^ and $ for multiple variables
+
+    @When("^user enters (.*) and (.*)$")
+    public void user_enters_username_and_password(String username, String password) throws InterruptedException {
+        System.out.println("Inside-Step: user enters username and password.");
+        driver.findElement(By.id("name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        Thread.sleep(2000);
+
+    }
+    @And("user click on login btn")
+    public void user_click_on_login_btn() throws InterruptedException {
+        System.out.println("Inside-Step: user click on login btn");
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(2000);
+    }
+
+    @Then("user is navigate to home page")
+    public void user_is_navigate_to_home_page() {
+        System.out.println("Inside-Step: user is navigate to home page");
+        driver.findElement(By.id("logout")).isDisplayed();
+        driver.close();
+        driver.quit();
+    }
+
+}
---
+++ target/test-classes/features/LoginTest.feature
@@ -0,0 +1,18 @@
+Feature: Test Login functionality
+
+
+    #  using the <> to parameterization the values
+    #  change the scenario to scenario outline
+    # add the examples
+    # pass the parameters from the function step inside the definitions step file.
+
+    Scenario Outline: Check Login is successful with valid credential
+    Given Browser is open.
+    And user is on login page
+    When user enters <username> and <password>.
+    And user click on login btn
+    Then user is navigate to home page
+    Examples:
+        | username | password |
+        |Latifa|12345|
+        |   Lat    | 123      |
